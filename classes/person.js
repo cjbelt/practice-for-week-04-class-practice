b91555ae6ce7ef0f3116b88030698c2cc12e393d
cjbelt <cjbeltran@protonmail.com>
2023-06-26 15:40:01 -0300

    class and its methods added

--- classes/person.js
+++ classes/person.js
@@ -1,4 +1,29 @@
 // Your code here
+class Person {
+  constructor(firstName, lastName, age) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+  }
+
+  introduce() {
+    console.log(`Hi, I'm ${this.firstName} ${this.lastName}, and I'm ${this.age} years old.`);
+  }
+
+  static introducePeople(arr) {
+    if (!Array.isArray(arr)) {
+      throw new Error("introducePeople only takes an array as an argument.");
+    }
+
+    arr.forEach((el) => {
+      if (!(el instanceof Person)) {
+        throw new Error("All items in array must be Person class instances.")
+      }
+
+      el.introduce();
+    })
+  }
+}
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
